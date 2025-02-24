eccc7bb61fd78546689923dd9b947738145f13ea
Desarrollo de Aplicaciones Web <ld.aguirreb@uea.edu.ec>
2025-02-23 19:26:48 -0500

    Semana 10 Plantillas de generación de contenido dinámico y reutilización de componentes.

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
 from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def home():
-    return '''
-        <h1>¡Hola! Bienvenido a mi Proyecto Web</h1>
-        <form action="/usuario" method="post">
-            <label for="nombre">Ingresa tu nombre:</label>
-            <input type="text" id="nombre" name="nombre" required>
-            <input type="submit" value="Enviar">
-        </form>
-    '''
+    return render_template('index.html')
 
 @app.route('/usuario', methods=['POST'])
 def usuario():
     nombre = request.form['nombre']
-    return f'<h1>Bienvenido, {nombre}!</h1>'
+    return render_template('usuario.html', nombre=nombre)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
